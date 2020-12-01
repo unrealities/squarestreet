69b2727b3a1815420dc6d4fe4b95035a849c294a
Tom Szymanski <szymanski.tom@gmail.com>
2020-12-01 12:12:56 -0700

    brain spewing

--- App.tsx
+++ App.tsx
@@ -4,7 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>What needs to be done?</Text>
+      <Text>Client</Text>
+      <Text>Should be able to login via Google, +++</Text>
+      <Text>Should show user what events they can enter</Text>
+      <Text>Should be able to enter an event if available</Text>
+      <Text>Should provide admin functionality for leagues and custom events</Text>
+      <Text>Server</Text>
+      <Text>Need to cache sporting event statuses</Text>
+      <Text>Need to persist users</Text>
+      <Text>Need to persist games</Text>
+      <Text>Need to persist leagues</Text>
     </View>
   );
 }
