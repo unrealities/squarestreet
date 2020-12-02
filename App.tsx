2cc725549ca74e4de5aa60a1c785b41ee2090517
Tom Szymanski <szymanski.tom@gmail.com>
2020-12-02 09:51:14 -0700

    baby stepping

--- App.tsx
+++ App.tsx
@@ -15,6 +15,13 @@ export default function App() {
       <Text>Need to persist users</Text>
       <Text>Need to persist games</Text>
       <Text>Need to persist leagues</Text>
+      <Text>MVP Baby Steps</Text>
+      <Text>User can login</Text>
+      <Text>User can see single available challenge with a single game</Text>
+      <Text>User can join challenge</Text>
+      <Text>User can pick one square</Text>
+      <Text>User can see what their numbers are</Text>
+      <Text>Pretend game can be played out and user alerted of status</Text>
     </View>
   );
 }
