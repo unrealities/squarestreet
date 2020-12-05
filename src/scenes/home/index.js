031201270e74bc1d42263d054ac8d27fc3e74bb2
Tom Szymanski <szymanski.tom@gmail.com>
2020-12-05 12:39:34 -0700

    basic screens

---
+++ src/scenes/home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const HomeScreen = () => (
+  <SafeAreaView>
+    <Text>Home Screen</Text>
+  </SafeAreaView>
+);
+
+export default HomeScreen;
---
+++ src/scenes/splash/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const SplashScreen = () => (
+  <SafeAreaView>
+    <Text>Splash Screen</Text>
+  </SafeAreaView>
+);
+
+export default SplashScreen;
