c675b29d7cf4762947e9080a4e85ad76e834436f
Tom Szymanski <szymanski.tom@gmail.com>
2020-12-08 11:23:29 -0700

    stack it up

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import * as SplashScreen from 'expo-splash-screen';
 
+import MyStack from './src/navigations/app-navigator.js'
+
 export default function App() {
   const [stuffToReturnHere, setLoadingComplete] = React.useState(false);
 
@@ -24,26 +26,7 @@ export default function App() {
 
 
   return (
-    <View style={styles.container}>
-      <Text>What needs to be done?</Text>
-      <Text>Client</Text>
-      <Text>Should be able to login via Google, +++</Text>
-      <Text>Should show user what events they can enter</Text>
-      <Text>Should be able to enter an event if available</Text>
-      <Text>Should provide admin functionality for leagues and custom events</Text>
-      <Text>Server</Text>
-      <Text>Need to cache sporting event statuses</Text>
-      <Text>Need to persist users</Text>
-      <Text>Need to persist games</Text>
-      <Text>Need to persist leagues</Text>
-      <Text>MVP Baby Steps</Text>
-      <Text>User can login</Text>
-      <Text>User can see single available challenge with a single game</Text>
-      <Text>User can join challenge</Text>
-      <Text>User can pick one square</Text>
-      <Text>User can see what their numbers are</Text>
-      <Text>Pretend game can be played out and user alerted of status</Text>
-    </View>
+    <MyStack />
   );
 }
 
