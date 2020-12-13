ea83a491aadd66df1592cf9ea0912d2812714ebb
Tom Szymanski <szymanski.tom@gmail.com>
2020-12-13 12:58:27 -0700

    I once knew how to do this

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { styles } from './src/styles/index.js';
 
 // import * as SplashScreen from 'expo-splash-screen';
 
@@ -35,12 +36,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
