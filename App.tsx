08515a4a081a528160add60a19ee0deaa81f7af9
Tom Szymanski <szymanski.tom@gmail.com>
2020-12-14 12:55:41 -0700

    hello world does not exist

--- App.tsx
+++ App.tsx
@@ -1,36 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import { styles } from './src/styles/index.js';
-
-// import * as SplashScreen from 'expo-splash-screen';
-
-// import MyStack from './src/navigations/app-navigator.js'
+import { styles } from './src/styles/';
 
 export default class App extends React.Component {
-  // const [stuffToReturnHere, setLoadingComplete] = React.useState(false);
-
-  /*
-  React.useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        // SplashScreen.preventAutoHideAsync();
-        // TODO: Load stuff here
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-        SplashScreen.hideAsync();
-      }
-    }
-
-    loadResourcesAndDataAsync();
-  }, []);
-  */
-
   render() {
-    console.log("is there anybody out there?");
+    console.log("redner in App");
     return (
-      <View>
+      <View style = { styles }>
         <Text>Testing</Text>
       </View>
     );
