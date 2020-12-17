5da3a353971b21891fb4f13ff02a12d78e609e7f
Tom Szymanski <szymanski.tom@gmail.com>
2020-12-17 09:12:32 -0700

    fiddling with export error

--- src/navigations/app-navigator.js
+++ src/navigations/app-navigator.js
@@ -4,7 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-const MyStack = () => {
+export default MyStack = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -18,5 +18,3 @@ const MyStack = () => {
     </NavigationContainer>
   );
 };
-
-export default MyStack;
