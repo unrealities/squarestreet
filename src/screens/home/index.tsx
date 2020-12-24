3095f6b12f525d9d22d9d646a2b2f03817c8f356
Tom Szymanski <szymanski.tom@gmail.com>
2020-12-24 08:33:04 -0700

    look I made a for loop

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -4,10 +4,16 @@ import { styles } from "../../styles";
 import { BaseCell } from "../../components/cell";
 
 export const HomeScreen = ({ navigation }) => {
+  const cells = [];
+
+  for (let i=0; i<=10; i++){
+    cells.push(<BaseCell id={i} />);
+  }
+
   return (
     <View style={styles.container}>
       <Text>Welcome To Square Street</Text>
-      <BaseCell id={1} />
+      {cells}
     </View>
   );
 };
