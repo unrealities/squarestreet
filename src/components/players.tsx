994d343af8b60c35551cce84f5935a54aa5984f1
Tom Szymanski <szymanski.tom@gmail.com>
2021-01-06 13:58:14 -0700

    add players

---
+++ src/components/players.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+// TODO:
+// The next step is to randomly generate a set of players and assign them a random amount of cells
+export class Players extends React.Component {}
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 import { styles } from "../../styles";
 import { CellGrid } from "../../components/cellGrid";
@@ -7,7 +7,6 @@ import { CellGrid } from "../../components/cellGrid";
 export const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Welcome To Square Street</Text>
       <CellGrid />
     </View>
   );
