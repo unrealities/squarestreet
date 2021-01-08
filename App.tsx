902d57e9f847b6ca2fe900b44fbe6397c7c4534c
Tom Szymanski <szymanski.tom@gmail.com>
2021-01-08 09:43:13 -0700

    thinking it over

--- App.tsx
+++ App.tsx
@@ -3,6 +3,11 @@ import { MyStack } from "./src/navigations/app-navigator";
 
 export default class App extends React.Component {
   render() {
+    // TODO: Generate random players with selections
+    const players = ["Abe","Betty","Carl","Diana","Edward"];
+
+    selections = [];
+
     return <MyStack />;
   }
 }
