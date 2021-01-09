7709ffe5531e36d9c175f0f898a25ef4df045530
Tom Szymanski <szymanski.tom@gmail.com>
2021-01-09 10:42:30 -0700

    service selection

---
+++ src/services/selection.tsx
@@ -0,0 +1,11 @@
+class Selection {
+  column: Number;
+  row: Number;
+
+  constructor(column: Number, row: Number) {
+    this.column = column;
+    this.row = row;
+  }
+}
+
+export default Selection;
