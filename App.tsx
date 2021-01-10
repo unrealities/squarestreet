b3eb471d89defb984ff286b551348ffcee212f44
Tom Szymanski <szymanski.tom@gmail.com>
2021-01-10 09:19:45 -0700

    doing it selection sunday

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { MyStack } from "./src/navigations/app-navigator";
 
+import Selection from "./src/services/selection";
+
 export default class App extends React.Component {
   render() {
     // TODO: Generate random players with selections
     const players = ["Abe","Betty","Carl","Diana","Edward"];
+    const selections = [];
 
-    selections = [];
+    for (let r = 0; r < 10; r++) {
+      for (let c = 0; c < 10; c++) {
+        let randPlayer = players[(Math.random() * players.length) | 0];
+        let s = new Selection(r, c, randPlayer);
+        selections.push(s);
+      }
+    }
+
+    console.log(selections);
 
     return <MyStack />;
   }
--- src/services/selection.tsx
+++ src/services/selection.tsx
@@ -1,11 +1,11 @@
-class Selection {
+export default class Selection {
   column: Number;
   row: Number;
+  player: String;
 
-  constructor(column: Number, row: Number) {
+  constructor(column: Number, row: Number, player: String) {
     this.column = column;
     this.row = row;
+    this.player = player;
   }
 }
-
-export default Selection;
