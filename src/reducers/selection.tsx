905f6436c2b3cba0a02f7e3c5c83ac3c326a8d09
Tom Szymanski <szymanski.tom@gmail.com>
2021-01-31 09:06:48 -0700

    need to update for typescript

--- src/reducers/selection.tsx
+++ src/reducers/selection.tsx
@@ -2,11 +2,23 @@ import { combineReducers } from 'redux';
 
 // TODO: https://www.digitalocean.com/community/tutorials/react-react-native-redux
 const INITIAL_STATE = {
-  selections: []
+  current: [],
+  possible: []
 };
 
 const selectionsReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case 'ADD_SELECTION':
+      const {
+        current,
+        possible
+      } = state;
+
+      const addedSelection = possible.splice(action.payload, 1);
+      current.push(addedSelection);
+      const newState = { current, possible };
+
+      return newState
     default:
       return state
   }
