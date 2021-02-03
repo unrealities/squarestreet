c009a5279e36c998c57cddadb275cdbb71505b9b
Tom Szymanski <szymanski.tom@gmail.com>
2021-02-03 09:13:59 -0700

    adding reducers

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import { MyStack } from "./src/navigations/app-navigator";
+import selectionReducer from './src/reducers/selection';
+
+const store = createStore(selectionReducer);
 
 export default class App extends React.Component {
   render() {
-    return <MyStack />;
+    return (
+      <Provider store={store}>
+        <MyStack />;
+      </Provider>
+    );
   }
 }
