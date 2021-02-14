0b4cffe555041a8c3fdd20770210f8fdfab74f9d
Tom Szymanski <szymanski.tom@gmail.com>
2021-02-14 09:53:50 -0700

    nope.  Screens are no good.

--- src/screens/selection/index.tsx
+++ src/screens/selection/index.tsx
@@ -2,45 +2,35 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators, mapStateToProps } from 'redux';
 import { Text, View, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 
 import { addSelection } from '../../actions/selection';
 import Selection from '../../services/selection';
 
-type SelectionScreenProps = {
-  selections: Selection[];
-  addSelection: Function;
-};
+export const SelectionScreen = ({ navigation, route }) => {
+  return (
+    <View>
+      <Text>Add Selections</Text>
 
-class SelectionScreen extends React.Component<SelectionScreenProps> {
-  render() {
-    const navigation = useNavigation();
+      {
+        this.props.selections.map((selection, index) => (
+          <Button
+            key={index}
+            title={`Add ${selection}`}
+            onPress={() =>
+              this.props.addSelection(index)
+            }
+          />
+        ))
+      }
 
-    return (
-      <View>
-        <Text>Add Selections</Text>
-
-        {
-          this.props.selections.map((selection, index) => (
-            <Button
-              key={index}
-              title={`Add ${selection}`}
-              onPress={() =>
-                this.props.addSelection(index)
-              }
-            />
-          ))
+      <Button
+        title="Back to home"
+        onPress={() =>
+          navigation.navigate('Home')
         }
-
-        <Button
-          title="Back to home"
-          onPress={() =>
-            navigation.navigate('Home')
-          }
-        />
-      </View>
-    );
-  }
+      />
+    </View>
+  );
 }
 
 const mapDispatchToProps = dispatch => (
